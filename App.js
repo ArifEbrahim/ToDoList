21aa3382ef687534941c59cc952dab615926b5fd
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-04-18 06:46:47 +0100

    feature: added basic components to App.js

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View>
+        <TextInput placeholder='Your to-do'/>
+        <Button title='Add to-do' />
+      </View>
+      <View>
+        <Text>List of to-do</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
