4893056a196beee4ac1ff8fb8308bb3802f88280
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-04-19 09:32:09 +0100

    feature: basic components and styling complete

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredToDoText, setEnteredToDoText] = useState("");
+  const [toDoItems, setToDoItems] = useState([]);
+
+  const toDoInputHandler = (enteredText) => {
+    setEnteredToDoText(enteredText);
+  };
+
+  const addToDoHandler = () => {
+    const updatedItems = [...toDoItems, enteredToDoText];
+    setToDoItems(updatedItems);
+  };
+
   return (
-    <View>
-      <View>
-        <TextInput placeholder='Your to-do'/>
-        <Button title='Add to-do' />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your to-do" onChangeText={toDoInputHandler} />
+        <Button title="Add to-do" onPress={addToDoHandler} />
       </View>
-      <View>
+      <View style={styles.toDoItemsContainer}>
         <Text>List of to-do</Text>
       </View>
     </View>
@@ -16,5 +29,21 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomColor: 'darkgrey',
+    borderBottomWidth: 1
+  },
+  toDoItemsContainer: {
+    flex: 5
+  },
 });
